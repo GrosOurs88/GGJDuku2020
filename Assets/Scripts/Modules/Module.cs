2a0f1125d9004eb22db4f1dcf4e38b2517162caf
Logan <logan.porati@gmail.com>
2020-02-01 17:52:54 +0100

    Ship and modules scripts

---
+++ Assets/Scripts/Modules/Module.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Module : MonoBehaviour
+{
+
+    public float lifePoints = 100;
+
+    public Ailments currentAilment = Ailments.None;
+
+    public bool requireEnergy = false;
+
+    public bool consumesEnergy = false;
+
+    public bool canBeDamaged = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum Ailments
+{
+    None,
+    Fire,
+}
---
+++ Assets/Scripts/ShipManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipManager : MonoBehaviour
+{
+    public float hullPoints = 100;
+
+    public Module[] moduleList;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
