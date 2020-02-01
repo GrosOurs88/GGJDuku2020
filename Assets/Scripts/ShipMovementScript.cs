66c11d6213bbe13807af4ccb9666fb805f24e554
Gros Ours <hugo.bourbon@wanadoo.fr>
2020-02-01 12:09:04 +0100

    Add script for ship movement

---
+++ Assets/Scripts/ShipMovementScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovementScript : MonoBehaviour
+{
+    public float speed;
+
+
+    void Update()
+    {
+        transform.position += new Vector3(0f, 0f, speed * Time.deltaTime);
+    }
+}
