4f593c23b546ce5b1aeaebdaba187e2e35665498
Gros Ours <hugo.bourbon@wanadoo.fr>
2020-02-01 18:41:54 +0100

    Add ligtingBolt FX + All sounds

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,24 +6,87 @@ public class SoundManager : MonoBehaviour
 {
     public AudioClip bipConfirm;
     public AudioClip bipDenied;
+
+    public AudioClip shock;
+    public AudioClip lighting;
+    public AudioClip fire;
+
+    public AudioClip fanOn;
     public AudioClip fanOff;
     public AudioClip fanReset;
 
-    public AudioSource enginesSource;
+    public AudioClip engineOn;
+    public AudioClip engineOff;
+    public AudioClip engineReset;
+
+    public AudioClip repartitorOff;
+    public AudioClip repartitorReset;
+
+    public AudioSource engineSource;
     public AudioSource fanSource;
+    public AudioSource sfxSource;
 
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        engineSource.clip = engineOn;
+        engineSource.Play();
+
+        fanSource.clip = fanOn;
+        fanSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }        
+    }
+
+    public void BipConfirm()
+    {
+        sfxSource.PlayOneShot(bipConfirm);
+    }
+    public void BipDenied()
+    {
+        sfxSource.PlayOneShot(bipDenied);
+    }
+    public void Shock()
+    {
+        sfxSource.PlayOneShot(shock);
+    }
+    public void Lighting()
+    {
+        sfxSource.PlayOneShot(lighting);
+    }
+
+
+    public void RepartitorOff()
+    {
+        sfxSource.PlayOneShot(repartitorOff);
+    }
+    public void RepartitorReset()
+    {
+        sfxSource.PlayOneShot(repartitorReset);
+    }
+
+
+    public void FanOff()
+    {
+        fanSource.Stop();
+    }
+    public void FanReset()
+    {
+        fanSource.Play();
+    }
+
+
+    public void EngineOff()
+    {
+        engineSource.Stop();
+    }
+    public void EngineReset()
+    {
+        engineSource.Play();
     }
 }
