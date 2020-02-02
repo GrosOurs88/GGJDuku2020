9d6a9813354e89c917e474e2092e68a7216a6c40
Logan <logan.porati@gmail.com>
2020-02-02 14:57:37 +0100

    serialize fields

--- Assets/Scripts/StateMachines/HealthEnum.cs
+++ Assets/Scripts/StateMachines/HealthEnum.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class HealthEnum : StateMachine<HealthEnum.HealthState>
 {
     public HealthEnum(HealthState defaultValue) : base(defaultValue)
