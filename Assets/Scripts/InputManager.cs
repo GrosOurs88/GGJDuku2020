dccb0562dc614275c4aee378cebed796ea8d89af
Logan <logan.porati@gmail.com>
2020-02-02 01:09:03 +0100

    InputManager -> click

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    public float inputDistance = 3;
+    public float inputDiameter = 1;
+    
+
+    private Camera camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int layerMask = LayerMask.GetMask("Clickable");
+            RaycastHit[] hit = Physics.SphereCastAll(camera.transform.position, inputDiameter, camera.transform.forward, inputDistance,
+                layerMask);
+
+            foreach (var raycastHit in hit)
+            {
+                raycastHit.collider.BroadcastMessage("OnClick");
+            }
+        }
+    }
+    
+}
