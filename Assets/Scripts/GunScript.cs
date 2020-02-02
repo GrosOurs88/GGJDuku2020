e4a8d26a8cb26fc2aa7b4fa0926febbb291b163f
Gros Ours <hugo.bourbon@wanadoo.fr>
2020-02-02 15:00:59 +0100

    Secret module

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public bool activateModule;
+    private bool allowfire = true;
+    public GameObject bullet;
+    public float fireRate = 0.25f;
+    public AudioSource shotSource;
+
+
+    public void Update()
+    {
+        if (activateModule)
+        {
+            if ((Input.GetMouseButton(0)) && allowfire)
+            {
+                StartCoroutine(Fire());
+            }
+        }
+    }
+
+    public IEnumerator Fire()
+    {
+        allowfire = false;
+
+        Instantiate(bullet, transform.position, transform.rotation);
+        shotSource.Play();
+
+        yield return new WaitForSeconds(fireRate);
+
+        allowfire = true;
+    }
+}
